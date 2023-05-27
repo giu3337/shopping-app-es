8fe6715bfed3d6c8c24ba0fdefcd5984831b93db
giu3337 <72093755+giu3337@users.noreply.github.com>
2023-05-26 20:51:19 -0500

    initial commit files

---
+++ index.js
@@ -0,0 +1,82 @@
+// Mobile app 
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push, onValue, remove , } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+// Firebase
+
+const appSettings = {
+    databaseURL: "https://realtime-database-27de7-default-rtdb.firebaseio.com/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListInDB = ref(database, "shoppingList")
+
+
+
+
+// App 
+
+const inputFieldEl = document.getElementById("input-field")
+const addButtonEl = document.getElementById("add-button")
+const shoppingListEl = document.getElementById("shopping-list")
+
+
+addButtonEl.addEventListener("click", function() {
+    let inputValue = inputFieldEl.value
+    push(shoppingListInDB, inputValue)
+    console.log(`${inputValue} added to database`)
+    clearInputFieldEl()
+})
+
+onValue(shoppingListInDB, function(snapshot) {
+
+    if (snapshot.exists()) {
+        let itemsArray = Object.entries(snapshot.val())
+
+    
+        clearShoppingListEl()
+    
+        for (let i = 0; i < itemsArray.length; i++) {
+            let currentItem = itemsArray[i] 
+            let currentItemId = currentItem[0]
+            let currentItemValue = currentItem[1]
+            
+            apprendItemToShoppingListEl(currentItem) 
+            
+        }
+    } else{
+        shoppingListEl.innerHTML = "No has agregado nada aún.."
+    }
+
+  
+}) 
+
+function clearShoppingListEl() {
+    shoppingListEl.innerHTML = ""
+}
+
+
+function clearInputFieldEl() {
+    inputFieldEl.value = ""
+}
+
+
+function apprendItemToShoppingListEl(item) {
+    let itemID = item[0]
+    let itemValue = item[1]
+
+    let newEl = document.createElement("li")
+    newEl.textContent = itemValue 
+
+    newEl.addEventListener('click', function () {
+        let exactLocationOfItemInDB = ref(database, `shoppingList/${itemID}`)
+        remove(exactLocationOfItemInDB)
+    })
+
+    shoppingListEl.append(newEl)
+
+    // shoppingListEl.innerHTML += `<li>${itemValue}</li>`
+}
+
